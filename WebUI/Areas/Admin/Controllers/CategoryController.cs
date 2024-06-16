52471c5e98e1a008a0ab3900318143edd7d8ddf4
svncmsva <sevincmusaeva01@gmail.com>
2024-06-16 20:18:31 +0400

    First commit

---
+++ WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,90 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Data;
+using WebUI.Models;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area(nameof(Admin))]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (string.IsNullOrEmpty(category.CategoryName))
+            {
+                ViewBag.Error = "The Category name is required!";
+                return View(category);
+            }
+            var findCategory=_context.Categories.FirstOrDefault(x=>x.CategoryName==category.CategoryName);
+            if (findCategory != null)
+            {
+                ViewBag.Error = "This category name is already exists!";
+                return View(findCategory);
+            }
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Redirect("/admin/category/index");
+        }
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var findCategories=_context.Categories.FirstOrDefault(x=>x.Id==id);
+            if (findCategories == null)
+            {
+                return NotFound();
+            }
+            return View(findCategories);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category) 
+        {
+
+            var findCategory = _context.Categories.FirstOrDefault(x => x.CategoryName.ToLower() == category.CategoryName.ToLower());
+            if (findCategory != null)
+            {
+                ViewBag.Error = "This category name is already exists!";
+                return View(findCategory);
+            }
+            _context.Categories.Update(category);
+                _context.SaveChanges();
+            return Redirect("/admin/category/index");
+        }
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            var findCategories = _context.Categories.FirstOrDefault(x => x.Id == id);
+            if (findCategories == null)
+            {
+                return NotFound();
+            }
+            return View(findCategories);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return Redirect("/admin/category/index");
+        }
+    }
+}
---
+++ WebUI/Areas/Admin/Controllers/TagController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebUI.Data;
+using WebUI.Models;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area(nameof(Admin))]
+    public class TagController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TagController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var tags = _context.Tags.ToList();
+            return View(tags);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Tag tag)
+        {
+            if (string.IsNullOrEmpty(tag.TagName))
+            {
+                ViewBag.Error = "The Tag name is required!";
+                return View(tag);
+            }
+            var findTag = _context.Tags.FirstOrDefault(x => x.TagName.ToLower() == tag.TagName.ToLower());
+            if (findTag != null)
+            {
+                ViewBag.Error = "This tag name is already exists!";
+                return View(findTag);
+            }
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
+            return Redirect("/admin/tag/index");
+        }
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var findTag = _context.Tags.FirstOrDefault(y => y.Id == id);
+            if (findTag == null)
+            {
+                return NotFound();
+            }
+            return View(findTag);
+        }
+        [HttpPost]
+        public IActionResult Edit(Tag tag)
+        {
+            var findTag = _context.Tags.FirstOrDefault(x => x.TagName.ToLower() == tag.TagName.ToLower());
+            if (findTag != null)
+            {
+                ViewBag.Error = "This tag name is already exists!";
+                return View(findTag);
+            }
+            _context.Tags.Update(tag);
+            _context.SaveChanges();
+            return Redirect("/admin/tag/index");
+
+        }
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            var deleteTag = _context.Tags.FirstOrDefault(y => y.Id == id);
+            if (deleteTag == null)
+            {
+                return NotFound();
+            }
+            return View(deleteTag);
+        }
+        [HttpPost]
+        public IActionResult Delete(Tag tag)
+        {
+           
+            _context.Tags.Remove(tag);
+            _context.SaveChanges();
+            return Redirect("/admin/tag/index");
+
+        }
+
+
+
+    }
+}
+
