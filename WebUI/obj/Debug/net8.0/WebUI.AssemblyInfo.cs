db06170f1cd42d200c607ed8144792b3573b507d
svncmsva <sevincmusaeva01@gmail.com>
2024-07-18 18:33:12 +0400

    deleted image

--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0224decd55804e72a3b525cfcd43b9a7598e15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934feb8f588b6301c1afe14ab02cc7ffece3c382")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
